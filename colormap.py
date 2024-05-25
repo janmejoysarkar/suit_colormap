0cf44db408a27a99842d6c6d4f3b54ecee3899d3
janmejoy <janmejoysarker@gmail.com>
2024-05-25 10:24:48 +0530

    All colormaps added

--- colormap.py
+++ colormap.py
@@ -10,6 +10,7 @@ from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 from astropy.io import fits
 import glob
+import numpy as np
 
 def make_cmap(col_ls, pos_ls=None):
     if (pos_ls != None): 
@@ -19,19 +20,23 @@ def make_cmap(col_ls, pos_ls=None):
     return(cmap)
 
 filterColor = {
+    'BB01': make_cmap(['#000000','#35095f','#e8acf3','#ffffff'], [0, 0.26, 0.7, 1]),
     'BB02': make_cmap(['#000000','#562210','#be8b12','#ffffff']),
+    'BB03': make_cmap(['#000000','#6d0000','#d39552','#ffffff']),
     'NB03': make_cmap(['#000000','#674201','#c1a50a','#ffffff']),
     'NB04': make_cmap(['#000000','#672000','#ffc600','#ffffff'], [0,0.34,0.68,1]), 
     'NB05': make_cmap(['#000000','#452263','#ab74d9','#ffffff'], [0,0.24,0.57,1]),
-    'NB06': make_cmap(['#000000','#005393','#68b0d6','#ffffff'], [0,0.38,0.66,1])
+    'NB06': make_cmap(['#000000','#005393','#68b0d6','#ffffff'], [0,0.38,0.66,1]),
+    'NB07': make_cmap(['#000000', '#aa4639', '#ffffff']),
+    'NB08': make_cmap(['#000000','#331385','#d2adff','#ffffff'])
 }
 
-ftr_name= 'NB06'
+ftr_name= 'NB08'
 file= glob.glob(f'/home/janmejoyarch/shared_folder/FITS_FOR_CHAIRMAN/*{ftr_name}.fits')[0]
 data=fits.open(file)[0].data
 
 plt.figure(ftr_name)
-plt.imshow(data, vmin=0, vmax= 7e4, cmap= filterColor[ftr_name])
+plt.imshow(data, vmin=0, vmax=1.5e4, cmap= filterColor[ftr_name])
 plt.colorbar()
 plt.show()
 
