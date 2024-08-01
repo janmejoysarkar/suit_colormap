764b88fd37bc96375b2067e26e9a59c9ae909f99
janmejoy <janmejoysarker@gmail.com>
2024-08-01 19:10:18 +0530

    Changed levels to solar orbiter campaign data

--- src/run.py
+++ src/run.py
@@ -17,25 +17,26 @@ if __name__=="__main__":
     project_path= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/SUIT_publicity/colormap_project')
     
     vmx_dict={"NB01":25000.0,
-    "NB02":50000.0,
-    "NB03":35000.0,
-    "NB04":40000.0,
-    "NB05":60000.0,
+    "NB02":17000.0,
+    "NB03":9000.0,
+    "NB04":10000.0,
+    "NB05":15000.0,
     "NB06":50000.0,
     "NB07":50000.0,
     "NB08":12000.0,
     "BB01":50000.0,
-    "BB02":35000.0,
+    "BB02":10000.0,
     "BB03":32000.0}
     
-    ftr_name='NB08'
-    file= glob.glob(f'{project_path}/data/*')[0]
+    ftr_name='NB05'
+    file= glob.glob(f'{project_path}/data/*1.0*{ftr_name}.fits')[0]
     data=fits.open(file)[0].data
+    print(os.path.basename(file))
     plt.figure(ftr_name)
     plt.imshow(data, vmin=0, vmax= vmx_dict[ftr_name], cmap= filterColor[ftr_name], origin='lower')
     plt.title(ftr_name)
     plt.colorbar()
-    if save==True: plt.savefig(f'{project_path}/products/{os.path.basename(file)}.png', dpi=600)
+    if save==True: plt.savefig(f'{project_path}/products/{os.path.basename(file)[:-5]}.png', dpi=600)
     plt.show()
 '''
     for ftr_name, vmx in zip(ftr_list, vmx_list):        
